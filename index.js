fd0b91c4675bced3a98e9ca71afc09fabc3cebc6
Nuuggs <bryluke000@gmail.com>
2021-10-28 10:34:39 +0800

    small changes

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import { writeFile } from 'fs';
 let name = process.argv[2];
 writeFile('data.json', name, (err) => console.error('err ->', err));
 console.log(`good day ${name}, are you well?`);
+
+// MAKING CHANGES HERE //
+console.log(`hehehe :>`);
